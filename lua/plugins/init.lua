a66e479d4b9a23e881bb013ea0ab90cdd0cec397
philipp <phil80301@berkeley.edu>
2026-03-07 18:07:38 -0800

    Revert "Fix arrow keys by disabling tmux-navigator default mappings"
    This reverts commit e1b246d1e8c42bd7217e13f5dd579e0a55939ccc.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -201,17 +201,9 @@ return {
     end,
   },
   { "christoomey/vim-tmux-navigator",
-    init = function()
-      vim.g.tmux_navigator_no_mappings = 1
-      vim.g.tmux_navigator_disable_when_zoomed = 1
-    end,
     config = function()
-      local opts = { silent = true, noremap = true }
-      vim.keymap.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>", opts)
-      vim.keymap.set("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>", opts)
-      vim.keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>", opts)
-      vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>", opts)
-    end,
+    vim.g.tmux_navigator_disable_when_zoomed= 1
+  end,
   },
   { "wookayin/semshi",
       ft = "python",
